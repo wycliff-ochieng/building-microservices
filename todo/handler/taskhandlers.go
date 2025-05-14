4ed1df06792d5f5a42ea23852c2e9bcd012a2b56
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-05-14 14:49:35 +0300

    adding tasks

--- todo/handler/taskhandlers.go
+++ todo/handler/taskhandlers.go
@@ -18,7 +18,7 @@ func NewTask(l *log.Logger) *Task {
 
 func (t Task) GetTasks(w http.ResponseWriter, r *http.Request) {
 
-	t.l.Println("method GET request")
+	t.l.Println("handling GET request")
 
 	lt := data.GetTasks()
 
@@ -29,6 +29,19 @@ func (t Task) GetTasks(w http.ResponseWriter, r *http.Request) {
 	w.Write(d)
 }
 
-func (t Task) AddTask(w http.ResponseWriter, r *http.Request) {
-	return
+type taskkey struct{}
+
+func (t *Task) AddTask(w http.ResponseWriter, r *http.Request) {
+
+	t.l.Println("handling POST requests")
+
+	tsk := r.Context().Value(taskkey{})
+	task, ok := tsk.(data.Task)
+	if !ok {
+		t.l.Println("task not found in context or tasklist")
+		http.Error(w, "invalid task", http.StatusInternalServerError)
+	}
+	data.AddTask(&task)
 }
+
+//handle middleware
