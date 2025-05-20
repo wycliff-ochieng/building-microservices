fdd1989d422cb926cda7d09976551cc95ec32d4c
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-05-20 17:03:12 +0300

    routes

--- currency/main.go
+++ currency/main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"net"
+	"os"
+
 	"github.com/hashicorp/go-hclog"
+	protos "github.com/wycliff-ochieng/currency/protos/currency"
+	"github.com/wycliff-ochieng/currency/server"
+	"google.golang.org/grpc"
 )
 
 func main() {
 
 	log := hclog.Default()
+
+	gs := grpc.NewServer()
+
+	cs := server.NewCurrency(log)
+
+	protos.RegisterCurrencyServer(gs, cs)
+
+	ls, err := net.Listen("tcp", ":9092")
+	if err != nil {
+		log.Error("Unable to listen to that network", err)
+	}
+	os.Exit(1)
+
+	gs.Serve(ls)
 
 }
